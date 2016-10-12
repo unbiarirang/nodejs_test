94e83327e4c5efac1f0a770c395897131e488737
최은비 <ebchoi@bulkytree.com>
2016-10-12 15:48:36 +0900

    이벤트 추가

--- test5.js
+++ test5.js
@@ -26,3 +26,24 @@ inputChecker.prototype.check = function check(input) {
     }
 };
 
+var ic = new inputCheck('Shelley', 'output');
+
+ic.on('write', function(data) {
+    this.writeStream.write(data, 'utf8');
+});
+
+ic.on('echo', function(data) {
+    console.log(this.name + ' wrote ' + data);
+});
+
+ic.on('end', function() {
+    process.exit();
+});
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function(input) {
+    ic.check(input);
+});
+
+

c60093e463e6a3c524ec87772a57cb12e04e7683
최은비 <ebchoi@bulkytree.com>
2016-10-13 18:06:43 +0900

    동기식 비동기식

--- test6.js
+++ test6.js
@@ -1,2 +1,23 @@
-var colors = require('colors');
-console.log('this text this this'.zebra);
+var fs2 = require('fs');
+var filenames2 = fs2.readdirSync('.');
+var i;
+
+for(i = 0; i< filenames2.length; i++) {
+    console.log(filenames2[i]);
+}
+
+console.log('ready 2');
+console.log('can process next job 2');
+
+var fs = require('fs');
+
+fs.readdir('.', function (err, filenames) {
+    var i;
+    for (i=0; i<filenames.length; i++) {
+        console.log(filenames[i]);
+    }
+
+    console.log('ready');
+});
+
+console.log('can process next job');

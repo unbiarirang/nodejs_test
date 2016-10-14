2434e1945aa89583c75710546cb5a22049387100
최은비 <ebchoi@bulkytree.com>
2016-10-14 12:20:37 +0900

    최종 콜백 기능의 기반 구조

---
+++ test7.js
@@ -0,0 +1,29 @@
+var obj = function() {};
+
+obj.prototype.doSomething = function(arg1, arg2_) {
+    var arg2 = (typeof(arg2_) === 'string' ? arg2_ : null);
+
+    var callback_ = arguments[arguments.length - 1];
+    callback = (typeof(callback_) === 'function' ? callback_ : null);
+
+    if(!arg2)
+        return callback(new Error('second argument missing or not a string'));
+
+    if(!callback)
+        return console.log('the last argument is not a function');
+
+    callback(arg1);
+}
+
+
+var test = new obj();
+
+try {
+    test.doSomething('test', '3.55', function(err, value) {
+        if(err) throw err;
+
+        console.log(value);
+    });
+} catch(err) {
+    console.log(err);
+}

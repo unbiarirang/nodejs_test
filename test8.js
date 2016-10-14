dcff5c7c91cb0f2aee0bbe646dce8510d097f4dd
최은비 <ebchoi@bulkytree.com>
2016-10-14 16:37:03 +0900

    async waterfall

---
+++ test8.js
@@ -0,0 +1,17 @@
+var async = require('async');
+
+var fs = require('fs');
+var src = './main.txt';
+var des = './output.txt';
+
+async.waterfall([
+    function (callback) {
+        fs.readFile(src, callback);
+    },
+    function (data, callback) {
+        fs.writeFile(des, data, callback);
+    }
+],
+function(err) {
+    if (err) console.log(err);
+});
